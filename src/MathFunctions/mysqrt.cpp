70395474086afac0243fe1866b599647696fcb0a
Taylor Price <trprice@gmail.com>
2012-10-27 19:38:54 -0700

    Added the log / exp method of computing the square root. The log/exp method isn't being called appropriately even though it looks like the definitions are correct.

--- src/MathFunctions/mysqrt.cpp
+++ src/MathFunctions/mysqrt.cpp
@@ -2,5 +2,10 @@
 
 double mysqrt (double dInput)
 {
-    return sqrt (dInput);
+    #if defined (HAVE_LOG) && defined (HAVE_EXP)
+        printf ("Using exp and log\n");
+        return 2 * (exp (log(x)*0.5));
+    #else
+        return sqrt (dInput);
+    #endif
 }
